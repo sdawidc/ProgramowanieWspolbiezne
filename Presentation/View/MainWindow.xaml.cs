78fdc34d921a93d462434647a210daf14a3c8a96
251494 Dawid Ciesielski <251494@edu.p.lodz.pl>
2025-03-25 17:22:09 +0100

    Project architecture initialization

--- Presentation/View/MainWindow.xaml.cs
+++ Presentation/View/MainWindow.xaml.cs
@@ -1,23 +1,42 @@
-﻿using System.Text;
+﻿//__________________________________________________________________________________________
+//
+//  Copyright 2024 Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and to get started
+//  comment using the discussion panel at
+//  https://github.com/mpostol/TP/discussions/182
+//__________________________________________________________________________________________
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Presentation.ViewModel;
 
-namespace Presentation;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace Presentation.View
 {
+  /// <summary>
+  /// View implementation
+  /// </summary>
+  public partial class MainWindow : Window
+  {
     public MainWindow()
     {
-        InitializeComponent();
+      Random random = new Random();
+      InitializeComponent();
+      MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+      double screenWidth = SystemParameters.PrimaryScreenWidth;
+      double screenHeight = SystemParameters.PrimaryScreenHeight;
+      viewModel.Start(random.Next(5, 10));
     }
+
+    /// <summary>
+    /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
+    /// </summary>
+    /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+    protected override void OnClosed(EventArgs e)
+    {
+      if (DataContext is MainWindowViewModel viewModel)
+        viewModel.Dispose();
+      base.OnClosed(e);
+    }
+  }
 }
